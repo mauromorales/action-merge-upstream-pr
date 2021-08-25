4cb1811b10abaf49cf2587b1558029ce54e72ed2
Mauro Morales <contact@mauromorales.com>
2021-08-25 22:27:52 +0200

    Switch entrypoint to fetch from upstream

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,12 +3,6 @@
 set -e
 set -x
 
-if [ -z "$INPUT_SOURCE_FOLDER" ]
-then
-  echo "Source folder must be defined"
-  return -1
-fi
-
 if [ $INPUT_DESTINATION_HEAD_BRANCH == "main" ] || [ $INPUT_DESTINATION_HEAD_BRANCH == "master"]
 then
   echo "Destination head branch cannot be 'main' nor 'master'"
@@ -32,25 +26,21 @@ git config --global user.name "$INPUT_USER_NAME"
 echo "Cloning destination git repository"
 git clone "https://$API_TOKEN_GITHUB@github.com/$INPUT_DESTINATION_REPO.git" "$CLONE_DIR"
 
-echo "Copying contents to git repo"
-mkdir -p $CLONE_DIR/$INPUT_DESTINATION_FOLDER/
-cp $INPUT_SOURCE_FOLDER "$CLONE_DIR/$INPUT_DESTINATION_FOLDER/"
 cd "$CLONE_DIR"
+git remote add upstream "https://$API_TOKEN_GITHUB@github.com/$GITHUB_REPOSITORY.git"
 git checkout -b "$INPUT_DESTINATION_HEAD_BRANCH"
 
-echo "Adding git commit"
-git add .
-if git status | grep -q "Changes to be committed"
-then
-  git commit --message "Update from https://github.com/$GITHUB_REPOSITORY/commit/$GITHUB_SHA"
-  echo "Pushing git commit"
-  git push -u origin HEAD:$INPUT_DESTINATION_HEAD_BRANCH
-  echo "Creating a pull request"
-  gh pr create -t $INPUT_DESTINATION_HEAD_BRANCH \
-               -b $INPUT_DESTINATION_HEAD_BRANCH \
-               -B $INPUT_DESTINATION_BASE_BRANCH \
-               -H $INPUT_DESTINATION_HEAD_BRANCH \
-                  $PULL_REQUEST_REVIEWERS
-else
-  echo "No changes detected"
-fi
+echo "Fetching upstream"
+git fetch upstream
+
+echo "Merging"
+git merge upstream/master --log -m 'Merge upstream/master'
+
+echo "Pushing git commit"
+git push -u origin HEAD:$INPUT_DESTINATION_HEAD_BRANCH
+echo "Creating a pull request"
+gh pr create -t $INPUT_DESTINATION_HEAD_BRANCH \
+              -b $INPUT_DESTINATION_HEAD_BRANCH \
+              -B $INPUT_DESTINATION_BASE_BRANCH \
+              -H $INPUT_DESTINATION_HEAD_BRANCH \
+                $PULL_REQUEST_REVIEWERS
